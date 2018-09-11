67d74c08bf0dd260ee09269ab33b5fd68c41e922
Takym <yamato0818@live.jp>
2018-09-10 19:15:26 -0700

    EditorWindow を追加

---
+++ Core/Editors/EditorWindow.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+using OSDeveloper.Core.GraphicalUIs;
+
+namespace OSDeveloper.Core.Editors
+{
+	/// <summary>
+	///  エディタを表すメインウィンドウに表示されるMDI子ウィンドウです。
+	/// </summary>
+	public class EditorWindow : Form
+	{
+		private readonly MainWindowBase _parent;
+		private new Form MdiParent { get; set; }
+
+		/// <summary>
+		///  型'<see cref="OSDeveloper.Core.Editors.EditorWindow"/>'の
+		///  新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="parent">親ウィンドウです。</param>
+		public EditorWindow(MainWindowBase parent)
+		{
+			_parent = parent;
+			base.MdiParent = parent;
+		}
+	}
+}
