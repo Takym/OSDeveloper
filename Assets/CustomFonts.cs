ec78eb2127a25c3d2e4cec92b5438f7e94ea38da
Takym <yamato0818@live.jp>
2018-11-11 13:45:21 -0800

    CustomFonts.cs追加。

---
+++ Assets/CustomFonts.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing.Text;
+using System.Globalization;
+using OSDeveloper.Core.FileManagement;
+using OSDeveloper.Core.Logging;
+
+namespace OSDeveloper.Assets
+{
+	internal static class CustomFonts
+	{
+		private static Logger _logger;
+		private static PrivateFontCollection _pfc;
+
+		static CustomFonts()
+		{
+			_logger = Logger.GetSystemLogger(nameof(CustomFonts));
+			_pfc = new PrivateFontCollection();
+			_pfc.AddFontFile(SystemPaths.Resources.Bond("IPAexfont00301\\ipaexg.ttf"));
+			_pfc.AddFontFile(SystemPaths.Resources.Bond("IPAexfont00301\\ipaexm.ttf"));
+
+			for (int i = 0; i < _pfc.Families.Length; ++i) {
+				var item = _pfc.Families[i];
+				string enus = item.GetName(CultureInfo.GetCultureInfo("en_US").LCID);
+				string jajp = item.GetName(CultureInfo.GetCultureInfo("ja_JP").LCID);
+				string cc = item.GetName(CultureInfo.CurrentCulture.LCID);
+				_logger.Info($"Loaded a font: {i}, {item.Name}; EN:{enus}; 日:{jajp}; ::: {cc}");
+			}
+		}
+	}
+}
