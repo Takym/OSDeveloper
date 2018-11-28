74c5f8610fc8cc6d57b1f269da6ff12cab805e4c
Takym <yamato0818@live.jp>
2018-11-27 20:09:42 -0800

    ダミーのエントリポイント追加＋少しだけja-JP追加

---
+++ Core/Settings/YenconTool/DummyEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSDeveloper.Core.Settings.YenconTool
+{
+	class DummyEntryPoint
+	{
+		static void Main() { }
+	}
+}
