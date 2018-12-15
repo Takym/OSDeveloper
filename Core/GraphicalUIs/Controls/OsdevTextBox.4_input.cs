da0028324fce15357ad3124ed650ebfcd475d72a
Takym <yamato0818@live.jp>
2018-12-15 12:47:07 -0800

    OsdevTextBox、ソースコード整理

---
+++ Core/GraphicalUIs/Controls/OsdevTextBox.4_input.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Forms;
+
+namespace OSDeveloper.Core.GraphicalUIs.Controls
+{
+	partial class __ { } // デザイナ避け
+	partial class OsdevTextBox
+	{
+		// TODO: OsdevTextBox.keyboard: 仮コード
+
+		/// <summary>
+		///  <see cref="System.Windows.Forms.Control.OnKeyDown"/>イベントを発生させます。
+		/// </summary>
+		/// <param name="e">
+		///  イベントデータを格納している<see cref="System.Windows.Forms.KeyEventArgs"/>オブジェクトです。
+		/// </param>
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			_logger.Trace($"executing {nameof(OnKeyDown)}...");
+			base.OnKeyDown(e);
+
+			switch (e.KeyCode) {
+				case Keys.Escape: { // ESCキー、コマンドタブを開く
+					e.Handled = true;
+					MainWindowBase.Current.OpenTerminalTab(this.CommandTab);
+				}
+				break;
+				case Keys.Tab: {
+
+				}
+				break;
+				default: {
+					e.Handled = true;
+					char k = ((char)(e.KeyCode & Keys.KeyCode));
+					if (k == '\b') {
+						this.RemoveStringFrom(_row_ss, _col_ss, 1);
+					} else {
+						this.AddStringTo(_row_ss, _col_ss, k.ToString());
+					}
+					++_col_ss;
+				}
+				break;
+			}
+
+			_logger.Trace($"completed {nameof(OnKeyDown)}");
+		}
+
+		/// <summary>
+		///  <see cref="System.Windows.Forms.Control.MouseWheel"/>イベントを発生させます。
+		/// </summary>
+		/// <param name="e">
+		///  イベントデータを格納している<see cref="System.Windows.Forms.MouseEventArgs"/>オブジェクトです。
+		/// </param>
+		protected override void OnMouseWheel(MouseEventArgs e)
+		{
+			_logger.Trace($"executing {nameof(OnMouseWheel)}...");
+
+			base.OnMouseWheel(e);
+			int v = vScrollBar.Value - e.Delta / 50;
+			if (v < vScrollBar.Minimum) v = vScrollBar.Minimum;
+			if (v > vScrollBar.Maximum) v = vScrollBar.Maximum;
+			vScrollBar.Value = v;
+			_row_sb = v;
+			this.Invalidate();
+
+			_logger.Trace($"completed {nameof(OnMouseWheel)}");
+		}
+
+		#region 未実装
+		/// <summary>
+		///  <see cref="System.Windows.Forms.Control.MouseDown"/>イベントを発生させます。
+		/// </summary>
+		/// <param name="e">
+		///  イベントデータを格納している<see cref="System.Windows.Forms.MouseEventArgs"/>オブジェクトです。
+		/// </param>
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			_logger.Trace($"executing {nameof(OnMouseDown)}...");
+
+			base.OnMouseDown(e);
+
+			_logger.Trace($"completed {nameof(OnMouseDown)}");
+		}
+
+		/// <summary>
+		///  <see cref="System.Windows.Forms.Control.MouseMove"/>イベントを発生させます。
+		/// </summary>
+		/// <param name="e">
+		///  イベントデータを格納している<see cref="System.Windows.Forms.MouseEventArgs"/>オブジェクトです。
+		/// </param>
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			_logger.Trace($"executing {nameof(OnMouseMove)}...");
+
+			base.OnMouseMove(e);
+
+			_logger.Trace($"completed {nameof(OnMouseMove)}");
+		}
+
+		/// <summary>
+		///  <see cref="System.Windows.Forms.Control.MouseUp"/>イベントを発生させます。
+		/// </summary>
+		/// <param name="e">
+		///  イベントデータを格納している<see cref="System.Windows.Forms.MouseEventArgs"/>オブジェクトです。
+		/// </param>
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			_logger.Trace($"executing {nameof(OnMouseUp)}...");
+
+			base.OnMouseDown(e);
+
+			_logger.Trace($"completed {nameof(OnMouseUp)}");
+		}
+		#endregion
+	}
+}
---
+++ Core/GraphicalUIs/Controls/OsdevTextBox.6_search.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using OSDeveloper.Core.Editors;
+
+namespace OSDeveloper.Core.GraphicalUIs.Controls
+{
+	partial class __ { } // デザイナ避け
+	partial class OsdevTextBox : ISearchReplaceFeature
+	{
+		public int Find(string str)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool FindNext(string str)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void ReplaceAll(string oldStr, string newStr)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public bool ReplaceNext(string oldStr, string newStr)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void ReplaceSelected(string newStr)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
