029ffaae871af19a51807ef37694ca3aee6d914a
Takym <yamato0818@live.jp>
2019-03-15 19:46:39 -0700

    TakymLib作成

---
+++ TakymLib/Collections/HybridList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakymLib.Collections
+{
+	public class HybridList<T> : IList<T>
+	{
+		public T this[int index]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public int Count
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public bool IsReadOnly
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public void Add(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Clear()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Contains(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int IndexOf(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(int index, T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Remove(T item)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RemoveAt(int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public class HybridEnumerator<T> : IEnumerator<T>
+		{
+			public T Current
+			{
+				get
+				{
+					throw new NotImplementedException();
+				}
+			}
+
+			object IEnumerator.Current
+			{
+				get
+				{
+					throw new NotImplementedException();
+				}
+			}
+
+			public bool MoveNext()
+			{
+				throw new NotImplementedException();
+			}
+
+			public void Reset()
+			{
+				throw new NotImplementedException();
+			}
+
+			#region IDisposable Support
+			private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+			protected virtual void Dispose(bool disposing)
+			{
+				if (!disposedValue) {
+					if (disposing) {
+						// TODO: マネージド状態を破棄します (マネージド オブジェクト)。
+					}
+
+					// TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
+					// TODO: 大きなフィールドを null に設定します。
+
+					disposedValue = true;
+				}
+			}
+
+			// TODO: 上の Dispose(bool disposing) にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
+			// ~HybridEnumerator() {
+			//   // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+			//   Dispose(false);
+			// }
+
+			// このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+			public void Dispose()
+			{
+				// このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+				Dispose(true);
+				// TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
+				// GC.SuppressFinalize(this);
+			}
+			#endregion
+		}
+	}
+}
--- TakymLib/Properties/AssemblyInfo.cs
+++ TakymLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("YenconCommandLineTool")]
-[assembly: AssemblyDescription("Manages a Yencon file on the console like a terminal shell.")]
+[assembly: AssemblyTitle("TakymLib")]
+[assembly: AssemblyDescription("This library is for me to use.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Takym")]
-[assembly: AssemblyProduct("yclt.exe")]
+[assembly: AssemblyProduct("TakymLib")]
 [assembly: AssemblyCopyright("Copyright (C) 2019 Takym.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("a39c90fa-d22f-49fc-8f29-1534fad0faea")]
+[assembly: Guid("640ff830-49c6-4e5d-b9c4-18e5b63398cb")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
-// 既定値にすることができます:
+// 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
