659131f71e112c14721c1d9d273bd010ffd25b3e
Takym <yamato0818@live.jp>
2019-04-18 11:01:40 -0700

    IUndoRedoFeature追加、メニューはまだ

---
+++ OSDeveloper/GraphicalUIs/Features/IUndoRedoFeature.cs
@@ -0,0 +1,10 @@
+﻿namespace OSDeveloper.GraphicalUIs.Features
+{
+	public interface IUndoRedoFeature
+	{
+		bool CanUndo { get; }
+		bool CanRedo { get; }
+		void Undo(int count = 1);
+		void Redo(int count = 1);
+	}
+}
