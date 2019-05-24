6113baf989c6cd0b2b5a4a4175dd7118a3aa1271
Takym <yamato0818@live.jp>
2019-05-23 19:46:10 -0700

    Yencon.Extension.MiscUtils に便利関数追加

--- Yencon/Extension/MiscUtils.cs
+++ Yencon/Extension/MiscUtils.cs
@@ -41,5 +41,133 @@ namespace Yencon.Extension
 				unchecked((int)((section.GetNode("w") as YNumber)?.SInt64Value)),
 				unchecked((int)((section.GetNode("h") as YNumber)?.SInt64Value)));
 		}
+
+		/// <summary>
+		///  <paramref name="section"/>から子セクションを取得します。
+		/// </summary>
+		/// <param name="section">取得元の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">取得する子セクションの名前です。</param>
+		/// <returns>
+		///  指定された名前のノードが、
+		///  子セクションの場合はそのセクションを、
+		///  それ以外の場合は<see langword="null"/>を返します。
+		/// </returns>
+		public static YSection GetNodeAsSection(this YSection section, string keyname)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YSection secKey) {
+				return secKey;
+			} else {
+				return null;
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>から文字列値を取得します。
+		/// </summary>
+		/// <param name="section">取得元の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">取得する文字列キーの名前です。</param>
+		/// <returns>
+		///  指定された名前のノードが、
+		///  文字列キーの場合はキーが保持している文字列を返し、
+		///  それ以外の場合は、キーの値を文字列に変換して返します。
+		/// </returns>
+		public static string GetNodeAsString(this YSection section, string keyname)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YString strKey) {
+				return strKey.Text;
+			} else {
+				return node.GetValue().ToString();
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>に文字列値を設定します。
+		/// </summary>
+		/// <param name="section">設定先の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">設定する文字列キーの名前です。</param>
+		/// <param name="value">設定する文字列です。</param>
+		public static void SetNodeAsString(this YSection section, string keyname, string value)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YString strKey) {
+				strKey.Text = value;
+			} else {
+				section.Add(new YString() { Name = keyname, Text = value });
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>から数値を取得します。
+		/// </summary>
+		/// <param name="section">取得元の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">取得する数値キーの名前です。</param>
+		/// <returns>
+		///  指定された名前のノードが、
+		///  数値キーの場合はキーが保持している符号付き64ビット数値を返し、
+		///  それ以外の場合は、<c>0</c>を返します。
+		/// </returns>
+		public static long GetNodeAsNumber(this YSection section, string keyname)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YNumber numKey) {
+				return numKey.SInt64Value;
+			} else {
+				return 0;
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>に数値を設定します。
+		/// </summary>
+		/// <param name="section">設定先の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">設定する数値キーの名前です。</param>
+		/// <param name="value">設定する数値です。</param>
+		public static void SetNodeAsNumber(this YSection section, string keyname, long value)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YNumber numKey) {
+				numKey.SInt64Value = value;
+			} else {
+				section.Add(new YNumber() { Name = keyname, SInt64Value = value });
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>から論理値を取得します。
+		/// </summary>
+		/// <param name="section">取得元の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">取得する論理値キーの名前です。</param>
+		/// <returns>
+		///  指定された名前のノードが、
+		///  論理値キーの場合はキーが保持している論理値を返し、
+		///  それ以外の場合は、<see langword="null"/>を返します。
+		/// </returns>
+		public static bool? GetNodeAsBoolean(this YSection section, string keyname)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YBoolean flgKey) {
+				return flgKey.Flag;
+			} else {
+				return null;
+			}
+		}
+
+		/// <summary>
+		///  <paramref name="section"/>に論理値を設定します。
+		/// </summary>
+		/// <param name="section">設定先の<see cref="Yencon.YSection"/>です。</param>
+		/// <param name="keyname">設定する論理値キーの名前です。</param>
+		/// <param name="value">設定する論理値です。</param>
+		public static void SetNodeAsBoolean(this YSection section, string keyname, bool value)
+		{
+			var node = section.GetNode(keyname);
+			if (node is YBoolean flgKey) {
+				flgKey.Flag = value;
+			} else {
+				section.Add(new YBoolean() { Name = keyname, Flag = value });
+			}
+		}
 	}
 }
--- Yencon/YenconFormatRecognition.cs
+++ Yencon/YenconFormatRecognition.cs
@@ -11,10 +11,29 @@ namespace Yencon
 	public static class YenconFormatRecognition
 	{
 		/// <summary>
+		///  ヱンコンとテキストの変換器を取得します。
+		/// </summary>
+		public static YenconStringConverter StringConverter { get; }
+
+		/// <summary>
+		///  ヱンコンとバイナリの変換器を取得します。
+		/// </summary>
+		public static YenconBinaryConverter BinaryConverter { get; }
+
+		static YenconFormatRecognition()
+		{
+			StringConverter = new YenconStringConverter();
+			BinaryConverter = new YenconBinaryConverter();
+		}
+
+		/// <summary>
 		///  指定されたファイルのヱンコンの種類を判定します。
 		/// </summary>
 		/// <param name="filename">判定するファイルのファイル名です。</param>
 		/// <returns>ヱンコンの種類を表す型'<see cref="Yencon.YenconType"/>'の値です。</returns>
+		/// <exception cref="System.IO.IOException">
+		///  ファイルの入力に失敗した場合に発生します。
+		/// </exception>
 		public static YenconType GetYenconType(string filename)
 		{
 			try {
@@ -38,6 +57,55 @@ namespace Yencon
 					}
 				}
 				return result;
+			} catch (Exception e) {
+				throw new IOException(string.Format(ErrorMessages.YenconFormatRecognition_IOException, filename), e);
+			}
+		}
+
+		/// <summary>
+		///  指定されたファイルを読み込みます。ヱンコンの種類は自動的に判別されます。
+		/// </summary>
+		/// <param name="filename">読み込むヱンコン環境設定ファイルの名前です。</param>
+		/// <returns>
+		///  指定されたファイルがヱンコン環境設定ファイルなら新しい型'<see cref="Yencon.YSection"/>'のインスタンス、
+		///  それ以外の場合は<see langword="null"/>を返します。
+		/// </returns>
+		/// <exception cref="System.IO.IOException">
+		///  ファイルの入力に失敗した場合に発生します。
+		/// </exception>
+		public static YSection Load(string filename)
+		{
+			var type = GetYenconType(filename);
+			try {
+				if (type == YenconType.Text) {
+					return StringConverter.Load(filename);
+				} else if (type == YenconType.Binary) {
+					return BinaryConverter.Load(filename);
+				} else {
+					return null;
+				}
+			} catch (Exception e) {
+				throw new IOException(string.Format(ErrorMessages.YenconFormatRecognition_IOException, filename), e);
+			}
+		}
+
+		/// <summary>
+		///  指定されたファイルに指定されたヱンコンオブジェクトを書き込みます。
+		/// </summary>
+		/// <param name="filename"><paramref name="obj"/>の保存先のファイル名です。</param>
+		/// <param name="obj">保存するヱンコンオブジェクトです。</param>
+		/// <param name="type">ヱンコンの種類です。既定でバイナリ形式です。</param>
+		/// <exception cref="System.IO.IOException">
+		///  ファイルの出力に失敗した場合に発生します。
+		/// </exception>
+		public static void Save(string filename, YSection obj, YenconType type = YenconType.Binary)
+		{
+			try {
+				if (type == YenconType.Text) {
+					StringConverter.Save(filename, obj);
+				} else if (type == YenconType.Binary) {
+					BinaryConverter.Save(filename, obj);
+				}
 			} catch (Exception e) {
 				throw new IOException(string.Format(ErrorMessages.YenconFormatRecognition_IOException, filename), e);
 			}
