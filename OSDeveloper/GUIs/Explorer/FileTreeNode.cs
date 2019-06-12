f95c57ade6f1ba6ecb681bd7c87952bdc80e901f
Takym <yamato0818@live.jp>
2019-06-12 11:02:32 -0700

    FileTreeNode を作成。

---
+++ OSDeveloper/GUIs/Explorer/FileTreeNode.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using OSDeveloper.GUIs.Editors;
+using OSDeveloper.GUIs.Terminal;
+using OSDeveloper.IO.ItemManagement;
+using OSDeveloper.Resources;
+
+namespace OSDeveloper.GUIs.Explorer
+{
+	public class FileTreeNode : TreeNode
+	{
+		public ItemMetadata Metadata { get; }
+		public ItemProperty Property { get; set; }
+		public EditorWindow Editor { get; set; }
+
+		public FolderMetadata Folder
+		{
+			get => this.Metadata as FolderMetadata;
+		}
+
+		public FileMetadata File
+		{
+			get => this.Metadata as FileMetadata;
+		}
+
+		public FileTreeNode(ItemMetadata meta)
+		{
+			this.Metadata = meta;
+			this.Text = meta.Name;
+		}
+	}
+
+	internal sealed class DummyTreeNode : TreeNode
+	{
+		public readonly static DummyTreeNode Instance = new DummyTreeNode();
+
+		private DummyTreeNode()
+		{
+			this.Text = this.GetType().FullName;
+			this.BackColor = Color.FromArgb(0xCC, 0xCC, 0xCC);
+			this.ForeColor = Color.FromArgb(0x22, 0x22, 0x22);
+		}
+	}
+
+	internal class RemovedTreeNode : TreeNode
+	{
+		public ItemMetadata Metadata { get; }
+
+		public RemovedTreeNode(ItemMetadata meta)
+		{
+			this.Metadata = meta;
+			this.Text = $"{ExplorerTexts.RemovedTreeNode} ({meta.Path})";
+		}
+	}
+}
