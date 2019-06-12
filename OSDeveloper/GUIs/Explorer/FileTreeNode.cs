affa37666b2fe31ec1bc6def46ba818e655409c2
Takym <yamato0818@live.jp>
2019-06-12 11:18:25 -0700

    FileTreeNode、スタイルを設定。

--- OSDeveloper/GUIs/Explorer/FileTreeNode.cs
+++ OSDeveloper/GUIs/Explorer/FileTreeNode.cs
@@ -1,17 +1,21 @@
 ﻿using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using OSDeveloper.GUIs.Editors;
 using OSDeveloper.GUIs.Terminal;
+using OSDeveloper.IO;
 using OSDeveloper.IO.ItemManagement;
+using OSDeveloper.IO.Logging;
 using OSDeveloper.Resources;
 
 namespace OSDeveloper.GUIs.Explorer
 {
 	public class FileTreeNode : TreeNode
 	{
-		public ItemMetadata Metadata { get; }
-		public ItemProperty Property { get; set; }
-		public EditorWindow Editor { get; set; }
+		protected Logger       Logger   { get; }
+		public    ItemMetadata Metadata { get; }
+		public    ItemProperty Property { get; set; }
+		public    EditorWindow Editor   { get; set; }
 
 		public FolderMetadata Folder
 		{
@@ -25,11 +29,131 @@ namespace OSDeveloper.GUIs.Explorer
 
 		public FileTreeNode(ItemMetadata meta)
 		{
+			this.Logger   = Logger.Get(this.GetType().Name);
 			this.Metadata = meta;
-			this.Text = meta.Name;
+			this.Text     = meta.Name;
+			this.SetStyle();
+		}
+
+		public void SetStyle()
+		{
+			if (this.Metadata.CanAccess) {
+				if (this.File != null) {
+					switch (this.File.Format) {
+						case FileFormat.Binary:
+							this.ImageIndex         = IconList.BinaryFile;
+							this.SelectedImageIndex = IconList.BinaryFile;
+							break;
+						case FileFormat.Text:
+							this.ImageIndex         = IconList.TextFile;
+							this.SelectedImageIndex = IconList.TextFile;
+							break;
+						case FileFormat.Program:
+							this.ImageIndex         = IconList.ProgramFile;
+							this.SelectedImageIndex = IconList.ProgramFile;
+							break;
+						case FileFormat.SourceCode:
+							this.ImageIndex         = IconList.SourceFile;
+							this.SelectedImageIndex = IconList.SourceFile;
+							break;
+						case FileFormat.Resource:
+							this.ImageIndex         = IconList.ResourceFile;
+							this.SelectedImageIndex = IconList.ResourceFile;
+							break;
+						case FileFormat.Document:
+							this.ImageIndex         = IconList.DocumentFile;
+							this.SelectedImageIndex = IconList.DocumentFile;
+							break;
+						case FileFormat.Solution:
+							this.ImageIndex         = IconList.SolutionFile;
+							this.SelectedImageIndex = IconList.SolutionFile;
+							break;
+						default:
+							this.ImageIndex         = IconList.File;
+							this.SelectedImageIndex = IconList.File;
+							break;
+					}
+				} else if (this.Folder != null) {
+					switch (this.Folder.Format) {
+						case FolderFormat.Directory:
+							if (this.Folder.IsEmpty()) {
+								this.ImageIndex         = IconList.Directory;
+								this.SelectedImageIndex = IconList.Directory;
+							} else if (this.IsExpanded) {
+								this.ImageIndex         = IconList.DirOpened;
+								this.SelectedImageIndex = IconList.DirOpened;
+							} else {
+								this.ImageIndex         = IconList.DirClosed;
+								this.SelectedImageIndex = IconList.DirClosed;
+							}
+							break;
+						case FolderFormat.FloppyDisk:
+							this.ImageIndex         = IconList.FloppyDisk;
+							this.SelectedImageIndex = IconList.FloppyDisk;
+							break;
+						case FolderFormat.HardDisk:
+							this.ImageIndex         = IconList.HardDisk;
+							this.SelectedImageIndex = IconList.HardDisk;
+							break;
+						case FolderFormat.OpticalDisc:
+							this.ImageIndex         = IconList.OpticalDisc;
+							this.SelectedImageIndex = IconList.OpticalDisc;
+							break;
+						case FolderFormat.Junction:
+							if (this.Folder.IsEmpty()) {
+								this.ImageIndex         = IconList.Junction;
+								this.SelectedImageIndex = IconList.Junction;
+							} else if (this.IsExpanded) {
+								this.ImageIndex         = IconList.JunOpened;
+								this.SelectedImageIndex = IconList.JunOpened;
+							} else {
+								this.ImageIndex         = IconList.JunClosed;
+								this.SelectedImageIndex = IconList.JunClosed;
+							}
+							break;
+						case FolderFormat.Solution:
+							this.ImageIndex         = IconList.Solution;
+							this.SelectedImageIndex = IconList.Solution;
+							break;
+						default:
+							if (this.Folder.IsEmpty()) {
+								this.ImageIndex         = IconList.Folder;
+								this.SelectedImageIndex = IconList.Folder;
+							} else if (this.IsExpanded) {
+								this.ImageIndex         = IconList.FolderOpened;
+								this.SelectedImageIndex = IconList.FolderOpened;
+							} else {
+								this.ImageIndex         = IconList.FolderClosed;
+								this.SelectedImageIndex = IconList.FolderClosed;
+							}
+							break;
+					}
+				}
+				byte r = 0, g = 0, b = 0;
+				if (this.Metadata.Attributes.HasFlag(FileAttributes.System)) {
+					g |= 0x80;
+				}
+				if (this.Metadata.Attributes.HasFlag(FileAttributes.Compressed) ||
+					this.Metadata.Attributes.HasFlag(FileAttributes.Encrypted)) {
+					b |= 0x80;
+				}
+				if (this.Metadata.Attributes.HasFlag(FileAttributes.Hidden) ||
+					this.Metadata.Attributes.HasFlag(FileAttributes.Temporary)) {
+					r |= 0x7F; g |= 0x7F; b |= 0x7F;
+				}
+				this.ForeColor = Color.FromArgb(r, g, b);
+			} else {
+				this.ImageIndex         = IconList.CannotAccess;
+				this.SelectedImageIndex = IconList.CannotAccess;
+				this.ForeColor          = Color.Red;
+			}
+
+			Logger.Notice($"finished to set styles to: {this.Metadata.Path}");
+			Logger.Info($"icon id:{this.ImageIndex}, color:{this.ForeColor}");
 		}
 	}
 
+#if false
 	internal sealed class DummyTreeNode : TreeNode
 	{
 		public readonly static DummyTreeNode Instance = new DummyTreeNode();
@@ -41,14 +165,12 @@ namespace OSDeveloper.GUIs.Explorer
 			this.ForeColor = Color.FromArgb(0x22, 0x22, 0x22);
 		}
 	}
+#endif
 
-	internal sealed class RemovedTreeNode : TreeNode
+	internal sealed class RemovedTreeNode : FileTreeNode
 	{
-		public ItemMetadata Metadata { get; }
-
-		public RemovedTreeNode(ItemMetadata meta)
+		public RemovedTreeNode(ItemMetadata meta) : base(meta)
 		{
-			this.Metadata = meta;
 			this.Text = $"{ExplorerTexts.RemovedTreeNode} ({meta.Path})";
 		}
 	}
