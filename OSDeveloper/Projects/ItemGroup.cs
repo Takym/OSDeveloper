b5579c983c45aefe93ae4c523c1073a918c27c5f
Takym <yamato0818@live.jp>
2019-06-13 18:44:01 -0700

    プロジェクト管理機能向上 (ItemGroup実装)

--- OSDeveloper/Projects/ItemGroup.cs
+++ OSDeveloper/Projects/ItemGroup.cs
@@ -1,34 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Yencon;
+using Yencon.Extension;
 
 namespace OSDeveloper.Projects
 {
 	public class ItemGroup : Project
 	{
+		public FolderKind Kind { get; set; }
+
 		public ItemGroup(Solution root, Project parent, string name) : base(root, parent, name) { }
+
+		public override void WriteTo(YSection section)
+		{
+			base.WriteTo(section);
+			this.Logger.Trace($"executing {nameof(ItemGroup)}.{nameof(this.WriteTo)} ({this.Name})...");
+			section.SetNodeAsString("FolderKind", this.Kind.ToString());
+			this.Logger.Trace($"completed {nameof(ItemGroup)}.{nameof(this.WriteTo)} ({this.Name})");
+		}
+
+		/// <exception cref="System.ArgumentException" />
+		public override void ReadFrom(YSection section)
+		{
+			base.ReadFrom(section);
+			this.Logger.Trace($"executing {nameof(ItemGroup)}.{nameof(this.ReadFrom)} ({this.Name})...");
+			var kind = ((FolderKind)(section.GetNodeAsNumber("FolderKind")));
+			_ = kind == FolderKind.Invalid && Enum.TryParse(section.GetNodeAsString("FolderKind"), out kind);
+			this.Kind = kind;
+			this.Logger.Trace($"completed {nameof(ItemGroup)}.{nameof(this.ReadFrom)} ({this.Name})...");
+		}
 
 		[Flags()]
 		public enum FolderKind
 		{
-			Input      = 0b__0_000_01,
-			Output     = 0b__0_000_10,
-			Compile    = 0b__0_001_00,
-			Asset      = 0b__0_010_00,
-			Executable = 0b__0_100_00,
-			Temporary  = 0b__1_000_00,
+			Invalid    = 0,
+
+			Input      = 0b__00000_01,
+			Output     = 0b__00000_10,
+			Compile    = 0b__00001_00,
+			Asset      = 0b__00010_00,
+			Program    = 0b__00100_00,
+			Publish    = 0b__01000_00,
+			Temporary  = 0b__10000_00,
 
 			SourceCode = Input | Compile,
 			Resource   = Input | Asset,
-			Document   = Input | Asset,
-			Library    = Input | Compile | Executable,
-			ToolKit    = Input | Executable,
+			Document   = Input | Asset | Publish,
+			Library    = Input | Compile | Program,
+			ToolKit    = Input | Program,
 			Object     = Output | Compile | Temporary,
 			Debug      = Output | Temporary,
-			Binary     = Output | Executable,
-			Package    = Output | Executable,
+			Binary     = Output | Program,
+			Package    = Output | Program | Publish,
 		}
 	}
 }
